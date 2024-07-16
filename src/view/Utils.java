e0effae1bf4aa318623229baf1a40e3572d4f44d
tanhuy2207 <tanhuytk2004@gmail.com>
2024-07-16 13:04:18 +0700

    third commit

--- src/view/Utils.java
+++ src/view/Utils.java
@@ -1,23 +1,36 @@
-package view;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+    
+    package view;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Utils {
-    public static String getValue(String input){
-        Scanner sc = new Scanner(System.in);
-        System.out.print(input);
-        return sc.nextLine();
+    public static String getString(String string){
+        System.out.print(string+" : ");
+        return new Scanner(System.in).nextLine();
     }
+//------------------------------------------------
+    public static LocalDate toLocalDate(String dob){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(dob, formatter);
+    }
+//------------------------------------------------
+    public static String toStringDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return formatter.format(date);
     
-    public static Date parseDate(String dateStr) {
-        try {
-            return new SimpleDateFormat("dd/MM/yyyy").parse(dateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
+}
+    
+
+ public static void print(String title, List list){
+        System.out.println(title);
+        System.out.println("------------------------------");
+        for(Object o:list) System.out.println(o); 
+        System.out.println("------------------------------");
+        System.out.println("Total :"+list.size());
     }
 }
